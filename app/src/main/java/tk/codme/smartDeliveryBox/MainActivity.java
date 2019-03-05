6a7d20f232260650d52c24c8871f85db14bb264b
jayapalreddy540 <jayapalreddymjpal@gmail.com>
2019-03-05 23:39:47 +0530

    otp via sms

---
+++ app/src/main/java/tk/codme/smartDeliveryBox/MainActivity.java
@@ -0,0 +1,139 @@
+package tk.codme.smartDeliveryBox;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
+
+    private Button sendOTP;
+    private TextView showOTP;
+    private EditText textOTP;
+    private DatabaseReference mRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendOTP = (Button) findViewById(R.id.sendOTP);
+        showOTP = (TextView) findViewById(R.id.showOTP);
+        textOTP = (EditText) findViewById(R.id.textOTP);
+
+        txtphoneNo = (EditText) findViewById(R.id.editText);
+
+        mRef= FirebaseDatabase.getInstance().getReference();
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    String txtOtp = dataSnapshot.child("number").getValue().toString();
+                    showOTP.setText("Present OTP : "+txtOtp);
+                }
+                catch(Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        sendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int OTP=Integer.parseInt(textOTP.getText().toString());
+
+                mRef.child("number").setValue(OTP).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        sendSMSMessage(OTP);
+                        Toast.makeText(MainActivity.this,"OTP setting successful",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+            }
+        });
+    }
+
+    protected void sendSMSMessage(int OTP) {
+        message = "Smart Delivery Box \n\n Your OTP : "+OTP;
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+        else{
+            phoneNo=txtphoneNo.getText().toString();
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "OTP sent to  : "+ phoneNo,
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "OTP sent via SMS.",Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS sending failed, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
+    }
+}
